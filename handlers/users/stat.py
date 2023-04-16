09ffa4033c88ed169b62b9eb8c78819ebbd08f87
Anvar Sobirov <asobirov9083@gmail.com>
2023-04-16 12:37:01 +0500

    Updated

--- handlers/users/stat.py
+++ handlers/users/stat.py
@@ -8,13 +8,8 @@ from keyboards.inline.admin import back
 @dp.callback_query_handler(text="statistika", state='*')
 async def statis(call: types.CallbackQuery, state: FSMContext):
     stat = await db.count_users()
-    statjson = await db_json.select_all_users()
     today = date.today()
 
-    if len(statjson) >= stat:
-        text = f"<b>📆 Bugunki sana: {today}\n\n📊 Bot obunachilari: {len(statjson)}\n\n⚡️@islamiuzbot</b>"
-        await call.message.edit_text(text=text, reply_markup=back)
-    else:
-        text = f"<b>📆 Bugunki sana: {today}\n\n📊 Bot obunachilari: {stat}\n\n⚡️@islamiuzbot</b>"
-        await call.message.edit_text(text=text, reply_markup=back)
+    text = f"<b>📆 Bugunki sana: {today}\n\n📊 Bot obunachilari: {stat}\n\n⚡️@islamiuzbot</b>"
+    await call.message.edit_text(text=text, reply_markup=back)
     await state.finish()
